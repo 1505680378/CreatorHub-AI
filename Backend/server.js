9d11ca7cd45aa108dd88072043fc9f3b97f986e0
1505680378 <rv3494998@gmail.com>
2026-01-19 13:01:46 +0530

    fix cors for production

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,16 @@ await connectCloudinary();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        process.env.FRONTEND_URL, // deployed frontend
+        'http://localhost:5173'   // local dev
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(clerkMiddleware());
 
